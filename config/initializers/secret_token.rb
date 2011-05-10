2e57ed79ef540b19c8cd55fe24b799aefb969cf0
Tam Tran <tam.tran@eastagile.com>
2011-05-10 11:31:47 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendsExplorerServerApp::Application.config.secret_token = '799efb60d09cd948b004f754989dc1bb6a1ceca38037876005586b6818eaed2b0788ee08558a73515d286550edd2fc02249a5294792efff159660881c4ec5929'
