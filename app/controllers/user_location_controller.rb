c4ae51077c3a5f9bbbacc5611a1f5c0f1449954a
Tam Tran <tam.tran@eastagile.com>
2011-05-11 11:29:01 +0700

    Server response XML data to Android App

--- app/controllers/user_location_controller.rb
+++ app/controllers/user_location_controller.rb
@@ -3,10 +3,21 @@ class UserLocationController < ApplicationController
     @nameDisplay = params[:name]
     @longDisplay = params[:long]
     @latDisplay = params[:lat]
-    @recordCreate = CurrentUsersLocation.create(:name => @nameDisplay,
-                                                :long => @longDisplay,
-                                                :lat  => @latDisplay
-    )
-    @successOrNot = @recordCreate.save
+    @recordChecking = CurrentUsersLocation.find_or_create_by_name(:name => @nameDisplay)
+    @recordChecking.long = @longDisplay
+    @recordChecking.lat = @latDisplay
+    @successOrNot = @recordChecking.save
+
+    @allLocation = CurrentUsersLocation.find(:all, :conditions => ["name NOT IN (?)", [@nameDisplay]])
+    render :inline => "<?xml version='1.0' encoding='ISO-8859-1'?>
+          <Location>
+            <% for location in @allLocation%>
+             <item>
+              <Name><%= location.name %> </Name>
+              <Long><%= location.long %> </Long>
+              <Lat><%= location.lat %> </Lat>
+             </item>
+          <% end %>
+          </Location>"
   end
 end
